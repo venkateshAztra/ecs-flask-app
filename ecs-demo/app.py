6ad6f2c53d02db52c909f577006bb1f3876e990a
venkateshAztra <venkatesh.koochana@aztra.ai>
2025-12-12 11:25:21 +0530

    Fix service name for ECS deploy

--- ecs-demo/app.py
+++ ecs-demo/app.py
@@ -1,8 +1,14 @@
-from flask import Flask
+from flask import Flask, request
+
 app = Flask(__name__)
+
+@app.before_request
+def log_request():
+    print(f"Incoming request: {request.method} {request.path} from {request.remote_addr}")
 
 @app.route("/")
 def hello():
-    return "Hello from ECS!v222"
+    print("Handling / request")   # Optional
+    return "Hello from ECS! v2222"
 
 app.run(host="0.0.0.0", port=8080)
